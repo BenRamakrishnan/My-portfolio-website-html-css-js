581ea29e933681500bac8d87591cdecfd37e7d1e
Ben Ramakrishnan <benramakrishnan8@gmail.com>
2025-09-02 19:12:08 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleManu() {
-    const manu = document.querySelector(".manu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    manu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
